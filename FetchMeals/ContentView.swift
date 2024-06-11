64ae07e51cd35c9dccb6195d50a22310881ab796
hauntarl <sameer.mungole@gmail.com>
2024-06-11 18:54:27 -0400

    Initial setup

---
+++ FetchMeals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FetchMeals
+//
+//  Created by Sameer Mungole on 6/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FetchMeals/FetchMealsApp.swift
@@ -0,0 +1,18 @@
+//
+//  FetchMealsApp.swift
+//  FetchMeals
+//
+//  Created by Sameer Mungole on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchMealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
